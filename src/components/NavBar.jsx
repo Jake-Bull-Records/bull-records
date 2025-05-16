30045263029fe7458aeb67c6c097e219dedcb5b3
Jake <jakebullrecords@gmail.com>
2025-05-15 22:48:13 -0400

    Fixing naming

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,42 +1,44 @@
-import {NavLink} from "react-router-dom";
-import logo from '../assets/logo.jpg';
-import './nav-bar.css'
+import { NavLink } from "react-router-dom";
+import logo from "../assets/logo.JPG";
+import "./nav-bar.css";
 
-function NavBar({session}){
-return(
-    <nav className='navbar'>
-          <div className='navbar-left'>
-            <NavLink to='/' className='logo'>
-                <img src={logo}></img> 
-                Bull Records
-            </NavLink>
-          </div>
-          <div className='navbar-center'>
-            <ul className='nav-links'>
-              <li>
-                <NavLink to='/'>Home</NavLink>
-              </li>
-              <li>
-                <NavLink to='/crate-digger'>Shop</NavLink>
-              </li>
-              <li>
-                <NavLink to='/About'>About</NavLink>
-              </li>
-              <li>
-                <NavLink to='/Contact'>Contact</NavLink>
-              </li>
-            </ul>
-          </div>
-          <div className='navbar-right'>
-            <NavLink to='/Profile' className='user-icon'>
-              <img src='/profile-user-account.svg'></img> 
-            </NavLink>
-            {session && (
-          <button onClick={async () => await supabase.auth.signOut()}>Sign Out</button>
+function NavBar({ session }) {
+  return (
+    <nav className="navbar">
+      <div className="navbar-left">
+        <NavLink to="/" className="logo">
+          <img src={logo}></img>
+          Bull Records
+        </NavLink>
+      </div>
+      <div className="navbar-center">
+        <ul className="nav-links">
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/crate-digger">Shop</NavLink>
+          </li>
+          <li>
+            <NavLink to="/About">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/Contact">Contact</NavLink>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-right">
+        <NavLink to="/Profile" className="user-icon">
+          <img src="/profile-user-account.svg"></img>
+        </NavLink>
+        {session && (
+          <button onClick={async () => await supabase.auth.signOut()}>
+            Sign Out
+          </button>
         )}
-          </div>
-        </nav>
-);
+      </div>
+    </nav>
+  );
 }
 
 export default NavBar;
